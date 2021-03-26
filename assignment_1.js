86f0bd1baeecec0fe05e2e99a915171177081473
Harshtiwari07 <57311514+Harshtiwari07@users.noreply.github.com>
2021-03-26 20:11:38 +0530

    Add files via upload

---
+++ assignment_1.js
@@ -0,0 +1,11 @@
+var h1=document.getElementsByTagName("h1");
+console.log(h1[0].innerText);
+
+var usa=document.getElementsByClassName('america')
+console.log(usa[0].innerText);
+
+var james=document.getElementById('007')
+console.log(james.innerText);
+
+
+usa[0].innerText="HARSH";
